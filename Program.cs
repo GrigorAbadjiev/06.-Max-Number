f5f7da6e4e238c280d2583b64b220468b0f82a75
User <User@DESKTOP-ESP2JT1>
2022-11-30 17:03:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double maxNumber = double.MinValue;
+
+            while (input != "Stop")
+            {
+                double inputAsNumber = double.Parse(input);
+
+                if (inputAsNumber > maxNumber)
+                {
+                    maxNumber = inputAsNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
